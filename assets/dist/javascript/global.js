5687e375fafba63560d5546277acc1d1fe36788b
Yulia <yulia.chaika@icloud.com>
2018-02-04 18:29:28 +0200

    bug fixes

--- assets/dist/javascript/global.js
+++ assets/dist/javascript/global.js
@@ -12485,27 +12485,22 @@ $.magnificPopup.registerModule(RETINA_NS, {
     $( function() {
 
 
-      ///
+      //magnific-popup for big img
       $('.modal-img-big').magnificPopup({
         type: 'image',
         closeOnContentClick: true,
         closeBtnInside: false,
-        // fixedContentPos: true,
-        // mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        mainClass: 'mfp-with-zoom', 
         image: {
           verticalFit: true
         },
         zoom: {
           enabled: true,
-          duration: 300 // don't foget to change the duration also in CSS
+          duration: 300 
         }
       });
 
 
-
-
-
-      
       /////
       var searchParams = new URLSearchParams(window.location.search);
       var targetHref = searchParams.get('t');
