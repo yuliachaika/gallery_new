9330b9342c5a11e1eb9ef52afdff617f954c2b37
Yulia <yulia.chaika@icloud.com>
2018-02-13 19:39:10 +0200

    bug fixes

---
+++ assets/src/javascript/plugins/fancybox.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function( $ ) {
+
+  $("[data-fancybox]").fancybox({     
+    afterShow: function() {
+          var $title = $(".modal-img__title").html();
+          console.log($title);
+          $(".fancybox-image-wrap").append('<div class="fancybox__title">' + $title + 
+            '</div>');
+            
+    },    
+    clickContent : function( current, event ) {
+      return 'close';
+    },
+    mobile : {
+
+      dblclickContent : function( current, event ) {
+        return current.type === 'image' ? 'close' : false;
+      }
+    }
+  });
+
+})( jQuery );
